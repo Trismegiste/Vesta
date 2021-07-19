dd04ec70589a1458959fea73ac025da7168bca38
Trismegiste <flo@trismegiste.net>
2021-07-19 10:57:56 +0200

    using new login system and testing logout

--- tests/Controller/SecurityTest.php
+++ tests/Controller/SecurityTest.php
@@ -6,6 +6,8 @@
 
 namespace App\Tests\Controller;
 
+use App\Tests\Command\CreateUserTest;
+
 /**
  * Description of SecurityTest
  *
@@ -14,13 +16,35 @@ namespace App\Tests\Controller;
 class SecurityTest extends \Symfony\Bundle\FrameworkBundle\Test\WebTestCase
 {
 
-    use \App\Tests\Controller\SecuredClientImpl;
-
     public function testLogin()
     {
-        $client = static::getAuthenticatedClient();
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/account/login');
+        $loginForm = $crawler->selectButton('Sign in')->form();
+        $client->submit($loginForm, [
+            'username' => CreateUserTest::username,
+            'password' => CreateUserTest::password,
+            'csrf_token' => $loginForm->get('csrf_token')->getValue(),
+        ]);
+
+        if ($client->getResponse()->headers->get('Location') === '/account/login') {
+            throw new RuntimeException("Bad configuration for TEST : authentication failed");
+        }
+
         $client->request('GET', '/');
         $this->assertPageTitleContains('Bienv');
+    }
+
+    public function testLogout()
+    {
+        $client = static::createClient();
+        $repo = static::$container->get('app.user.repository');
+        $iter = $repo->search(['username' => CreateUserTest::username]);
+        $user = iterator_to_array($iter)[0];
+        $client->loginUser($user);
+        $this->assertNotNull($client->getContainer()->get('session')->get('_security_main'));
+        $client->request('GET', '/logout');
+        $this->assertNull($client->getContainer()->get('session')->get('_security_main'));
     }
 
 }
