3a94b68abf7410cb1a399b24def42e236b9b9bf4
Trismegiste <flo@trismegiste.net>
2021-07-20 11:54:39 +0200

    skeleton for listingt

---
+++ src/Controller/Buy.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * Vesta
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Description of Buy
+ *
+ * @author flo
+ */
+class Buy extends AbstractController
+{
+
+    /**
+     * @Route("/listing", methods={"GET"})
+     */
+    public function listing(): Response
+    {
+        $listing = ['e'];
+        return $this->render('front/listing.html.twig', ['result' => $listing]);
+    }
+
+}
