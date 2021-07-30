1215df3d37f58c84aa73fa23ced82ca347b8e100
Trismegiste <flo@trismegiste.net>
2021-07-30 17:00:36 +0200

    testing form et externalize socnet

--- src/Controller/Sell.php
+++ src/Controller/Sell.php
@@ -7,6 +7,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,12 +19,13 @@ class Sell extends AbstractController
 {
 
     /**
-     * @Route("/sell", methods={"GET"})
+     * @Route("/sell", methods={"GET","POST"})
      */
     public function create(): Response
     {
         $form = $this->createFormBuilder()
                 ->add('email', TextType::class)
+                ->add('Envoi', SubmitType::class)
                 ->getForm();
 
         return $this->render('front/seller/create.html.twig', ['form' => $form->createView()]);
