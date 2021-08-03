e5167d454a1964837e64d5be41934ce8830b8330
Trismegiste <flo@trismegiste.net>
2021-08-03 11:46:27 +0200

    func test

--- tests/Controller/BuyTest.php
+++ tests/Controller/BuyTest.php
@@ -14,9 +14,22 @@ class BuyTest extends WebTestCase
     public function testSearchEmpty()
     {
         $client = static::createClient();
-        $client->request('GET', '/search');
+        $crawler = $client->request('GET', '/search');
         $this->assertResponseIsSuccessful();
         $this->assertPageTitleContains('Recherchez');
+        $buttonCrawlerNode = $crawler->selectButton('search[search]');
+        $this->assertCount(1, $buttonCrawlerNode);
+    }
+
+    public function testSearchCity()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/search');
+        $buttonCrawlerNode = $crawler->selectButton('search[search]');
+        $form = $buttonCrawlerNode->form();
+        $form['search[city]'] = 'nissa';
+        $client->submit($form);
+        $this->assertPageTitleContains('Résultat');
     }
 
     public function testVisit()
