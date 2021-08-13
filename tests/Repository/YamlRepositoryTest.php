4ddef7b30d0150ec15832cbc72aeea75fb1174f5
Trismegiste <flo@trismegiste.net>
2021-08-13 15:57:37 +0200

    unit test for YML repo

---
+++ tests/Repository/YamlRepositoryTest.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * Vesta
+ */
+
+use App\Repository\YamlRepository;
+use PHPUnit\Framework\TestCase;
+
+class YamlRepositoryTest extends TestCase
+{
+
+    protected $sut;
+
+    protected function setUp(): void
+    {
+        $this->sut = new YamlRepository(join_paths(__DIR__, 'test_repo.yml'));
+    }
+
+    public function testFindAll()
+    {
+        $listing = $this->sut->findAll('section9');
+        $this->assertCount(3, $listing);
+        $this->assertArrayHasKey('Motoko', $listing);
+        $this->assertEquals('Motoko', $listing['Motoko']);
+    }
+
+}
