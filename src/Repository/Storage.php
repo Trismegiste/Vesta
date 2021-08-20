6dc5ec78d0e4dc1275efd09e30684146b8bd8152
Trismegiste <flo@trismegiste.net>
2021-08-20 16:06:02 +0200

    test download

--- src/Repository/Storage.php
+++ src/Repository/Storage.php
@@ -6,12 +6,16 @@
 
 namespace App\Repository;
 
+use Exception;
 use MongoDB\BSON\ObjectIdInterface;
 use MongoDB\Database;
 use MongoDB\Driver\Manager;
 use Psr\Log\LoggerInterface;
 use Psr\Log\NullLogger;
+use RuntimeException;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 /**
  * This is a storage engine on MongoDB
@@ -33,10 +37,29 @@ class Storage
     {
         $stream = fopen($uf->getPathname(), 'rb');
         $meta['mime'] = $uf->getMimeType();
-        $id = $this->bucket->uploadFromStream($uf->getFilename(), $stream, ['metadata' => $meta]);
-        fclose($stream);
+
+        try {
+            $id = $this->bucket->uploadFromStream($uf->getFilename(), $stream, ['metadata' => $meta]);
+            fclose($stream);
+        } catch (Exception $e) {
+            throw RuntimeException("Unable to store " . $uf->getClientOriginalName(), 500, $e);
+        }
 
         return $id;
+    }
+
+    public function get(ObjectIdInterface $pk): Response
+    {
+        $stream = $this->bucket->openDownloadStream($pk);
+        $metadata = $this->bucket->getFileDocumentForStream($stream);
+
+        $response = new StreamedResponse(function () use ($stream) {
+                    fpassthru($stream);
+                });
+        $response->setImmutable();
+        $response->setDate($metadata->uploadDate->toDateTime());
+
+        return $response;
     }
 
 }
--- tests/Repository/StorageTest.php
+++ tests/Repository/StorageTest.php
@@ -4,6 +4,7 @@ use App\Repository\Storage;
 use MongoDB\BSON\ObjectIdInterface;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 /*
  * Vesta
@@ -20,10 +21,22 @@ class StorageTest extends KernelTestCase
         $this->sut = static::getContainer()->get('app.storage');
     }
 
-    public function testWrite()
+    public function testUpload()
     {
         $pk = $this->sut->storeUploaded(new UploadedFile(__FILE__, 'yolo.txt'));
         $this->assertInstanceOf(ObjectIdInterface::class, $pk);
+
+        return $pk;
+    }
+
+    /**
+     * @depends testUpload
+     */
+    public function testDownlad($pk)
+    {
+        $response = $this->sut->get($pk);
+        $this->assertInstanceOf(StreamedResponse::class, $response);
+        $response->sendContent();
     }
 
 }
